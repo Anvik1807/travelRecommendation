f65c1af8c019707306de72dbd90219418673bb8c
Your Name <you@example.com>
2024-12-15 09:41:33 -0500

    changes

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -13,14 +13,26 @@ function searchTravelPlaces(){
       // const condition = data.Travel.find(item => item.name.toLowerCase() === input.toLowerCase());
       const condition = data.Travel.find(item => item.name.toLowerCase().includes(input.toLowerCase()));
       //string.toLowerCase().includes(substring.toLowerCase())
+      console.log(condition);
        if(condition){
         console.log(condition);
-        // const options = { timeZone: condition.timezone, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
-        // const timezone = new Date().toLocaleTimeString('en-US', options); 
-        // result.innerHTML = `<h2>${condition.name.toUpperCase()}</h2>`;
-        // result.innerHTML += `<img src="${condition.imagesrc}"/>`;
-        // result.innerHTML += `<div>${condition.description}</h2>`;
-        // result.innerHTML += `<div>Current Time in ${condition.name} is: ${timezone}</div>`
+        console.log(condition.places);
+        let places = condition.places;
+        result.innerHTML = '';
+        places.forEach(function(data, index) {
+        if(data.timezone){
+        var options = { timeZone: data.timezone, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+        var timezone = new Date().toLocaleTimeString('en-US', options); 
+        }
+        result.innerHTML += `<h2>${data.name.toUpperCase()}</h2>`;
+        result.innerHTML += `<img src="${data.imagesrc}"/>`;
+        result.innerHTML += `<div>${data.description}</h2>`;
+        if(timezone){
+        result.innerHTML += `<div>Current Time in ${data.name} is: ${timezone}</div>`;
+        } else if(data.place){
+        result.innerHTML += `<div>Place: ${data.place}</div>`;    
+        }
+        });
        } else {
         result.innerHTML = `<h2 style="color: red;">Travel Place Not found</h2>`;
        }
